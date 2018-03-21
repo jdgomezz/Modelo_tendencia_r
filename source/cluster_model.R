d0c754577a10b473d0c6a22130426eecae5fedad
Jesus <jdgomezz@UbuntuMLServer.hpyefbkufequrhxfrogwlnbprg.bx.internal.cloudapp.net>
2018-03-21 22:15:04 +0000

    Modificación de scripts de h2o

--- source/cluster_model.R
+++ source/cluster_model.R
@@ -1,14 +1,20 @@
 
-cluster_model <- function(chars, k_n, nth, memo){
+cluster_model <- function(xs, k_n, nth, memo, dep){
+  
+  k_n <- 100
+  nth <- 10
+  memo <- '4g'
   conn <- h2o.init(ip = "localhost", port=54321, nthreads = nth, max_mem_size = memo)
   h2o.removeAll() # Clean slate - just in case the cluster was already running
   
-  chars.hex <- as.h2o(chars);          # Conversión a formato de datos de h2o
-  chars.hex[,1] <- as.factor(chars.hex[,1]) # Plus
-  chars.hex[,2] <- as.factor(chars.hex[,2]) # mes
-  chars.hex[,3] <- as.factor(chars.hex[,3]) # dia
+  xs.hex <- as.h2o(xs);          # Conversión a formato de datos de h2o
   
-  splits <- h2o.splitFrame(chars.hex,           ##  splitting the H2O frame we read above
+  xs.hex <- xs.hex[xs.hex[, "DependenciaCD"] == dep, ]
+  xs.hex[,1] <- as.factor(xs.hex[,1]) # Dependencia
+  xs.hex[,2] <- as.factor(xs.hex[,2]) # Plu
+  xs.hex[,3] <- as.factor(xs.hex[,3]) # dia
+  
+  splits <- h2o.splitFrame(xs.hex,           ##  splitting the H2O frame we read above
                            c(0.6, 0.2),         ## create splits of 60% and 20%; 
                                                 ##  H2O will create one more split of 1-(sum of these parameters)
                                                 ##  so we will get 0.6 / 0.2 / 1 - (0.6+0.2) = 0.6/0.2/0.2
@@ -18,12 +24,13 @@ cluster_model <- function(chars, k_n, nth, memo){
   valid <- h2o.assign(splits[[2]], "valid.hex")   ## R valid, H2O valid.hex
   test <- h2o.assign(splits[[3]], "test.hex")     ## R test, H2O test.hex
   
-  y <- c("m1", "m2", "s", "sd", "moda", "q1", "q2", "q3", "asim_fisher", "curtosis", "asim_pearson1", "asim_pearson2", "asim_bowley");
+  y <- c("m1", "m2", "m3", "sd", "moda", "q1", "q2", "q3", "sesgo", "curtosis", "asim_fisher", "asim_pearson0",  "asim_pearson", "asim_bowley", "m1h", "m2h", "m3h", "sdh",  "modah", "q1h", "q2h", "q3h", "sesgoh", "curtosish", "asim_fisherh",   "asim_pearson0h", "asim_pearsonh",  "asim_bowleyh")
+
   x <- setdiff(names(train), y);  #vector of predictor column names
   x <- x[1:3];                    # Variables independientes: dep / plu / dia
   
   # k-means
-  cluster_model = h2o.kmeans(training_frame = train, k = k_n, estimate_k = TRUE, x =  1:13)
+  cluster_model = h2o.kmeans(training_frame = train, k = k_n, x =  y)
   centers <- as.matrix(cluster_model@model$centers);
   nc = nrow(centers)
   distance <- matrix(nrow = nc, ncol = nc)
@@ -40,13 +47,14 @@ cluster_model <- function(chars, k_n, nth, memo){
   chosen <- matrix(nrow = nc, ncol = 1)
   
   # Find closest points to centroids
+  N <- ncol(centers)
   for (j in 1:nc){
     d <- Inf;
-    for (i in 1:nrow(chars.hex)){
-      x <- as.numeric(centers[j, 2:15]);
-      y <- as.vector(as.numeric(chars.hex[i, 4:17]));
-      y[is.na(y)] <- 0;
-      d_aux <- sqrt(sum((x - y)^2));
+    for (i in 1:nrow(xs.hex)){
+      x <- as.numeric(centers[j, 2:N]);
+      yy <- as.vector(as.numeric(xs.hex[i, y]));
+      yy[is.na(yy)] <- 0;
+      d_aux <- sqrt(sum((x - yy)^2));
       if (d_aux < d){
         d <- d_aux;
         chosen[j, 1] <- i
