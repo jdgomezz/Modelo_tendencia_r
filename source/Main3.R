bac72d044f2ccfa62b625df90f1c17c4cbd3603e
Jesus <jdgomezz@UbuntuMLServer.hpyefbkufequrhxfrogwlnbprg.bx.internal.cloudapp.net>
2018-04-18 23:05:37 +0000

    Main3 v5

--- source/Main3.R
+++ source/Main3.R
@@ -100,10 +100,10 @@ dias <- c(Sys.Date()- as.POSIXlt(Sys.Date())$wday,
 fecha_dia = data.frame(dia = 1:7, diafecha = dias)
 
 outfile <- merge(x = outfile, y = fecha_dia, by = "dia", all.x = TRUE)
-outfile <- subset(outfile, select = c(11, 3, 2, 4, 7))
-colnames(outfile) <- c("Fecha", "Pluid", "storeid", "Hora", "Perfil")
+outfile <- subset(outfile, select = c(11, 3, 2, 4, 7, 5, 9))
+colnames(outfile) <- c("Fecha", "Pluid", "storeid", "Hora", "P1", "P2", "P3")
 
-outfile[is.na(outfile[, 5]), 5] <- 0
+outfile[is.na(outfile[, 5]), c(5, 6, 7)] <- 0
 outfile <- outfile[order(outfile$Fecha, outfile$storeid, outfile$Pluid, outfile$Hora), ]# Escritura de archivo final
 
 outfile$Fecha <- format(outfile$Fecha, "%Y-%m-%d")
@@ -163,18 +163,101 @@ outfile_d <- merge(x = outfile_c,
                    all.x = TRUE)
 
 outfile_d[is.na(outfile_d$Perfil.y), 6] <- 0
-outfile_d[is.na(outfile_d$UnidadesVendidas.x), 8] <- 0
-outfile_d[is.na(outfile_d$UnidadesVendidas.y), 9] <- 0
+outfile_d[is.na(outfile_d$UnidadesVendidas.x), 10] <- 0
+outfile_d[is.na(outfile_d$UnidadesVendidas.y), 11] <- 0
 
 outfile_d$Proy <- outfile_d$UnidadesVendidas.y*ifelse(outfile_d$Perfil.y == 0, 0, (outfile_d$Perfil.x/outfile_d$Perfil.y))
-outfile_d$abse <- abs(outfile_d$Proy - outfile_d$UnidadesVendidas.x)
+
+outfile_d$abs_proy <- abs(outfile_d$UnidadesVendidas.x-outfile_d$Proy)
+outfile_d$abs_proy2 <- abs(outfile_d$UnidadesVendidas.x-outfile_d$P1)
+outfile_d$abs_proy3 <- abs(outfile_d$UnidadesVendidas.x-outfile_d$P2)
+outfile_d$abs_proy4 <- abs(outfile_d$UnidadesVendidas.x-outfile_d$P3)
+
+query_prods <- "SELECT SUBLINEA_ID, SUBLINEA_DESC, CATEGORIA_ID, CATEGORIA_DESC, SUBCATEGORIA_ID, SUBCATEGORIA_DESC, MARCA_ID, MARCA_DESC, PLUID, PLUCD, PLU_DESC FROM bd_ddpo.NVM_PRODUCTS_A where GEN_ID = 40 and PLUVIGENTE = 1"
+
+connectionString <-"Driver=Teradata;DBCNAME=10.2.113.66;UID=jdgomezz;PWD=jdgomezz01;"
+odbcDS <-RxOdbcData(sqlQuery = query_prods,connectionString = connectionString)
+products <- rxImport(odbcDS)
+
+query_deps <- "SELECT LocCD, TIENDA_DC_DESC, CADENA_DESC FROM bd_ddpo.NVM_LOCATION_A"
+odbcDS <-RxOdbcData(sqlQuery = query_deps,connectionString = connectionString)
+deps <- rxImport(odbcDS)
+
+outfile_d <- merge(x = outfile_d,
+                   y = products, 
+                   by.x  = c("Pluid"),
+                   by.y = c("PluID"),
+                   all.x = TRUE)
+
+outfile_d <- merge(x = outfile_d,
+                   y = deps, 
+                   by.x  = c("storeid"),
+                   by.y = c("LocCD"),
+                   all.x = TRUE)
+
+outfile_d <- outfile_d[order(outfile_d$Fecha, outfile_d$Pluid, outfile_d$storeid, outfile_d$Hora), ]
+
+write.table(outfile_d, file = paste0(output_lib, "modelo_deps.csv"), sep=",", row.names = FALSE)
+write.table(outfile_aux, file = paste0(output_lib, "modelo_deps.csv"), sep=",", row.names = FALSE)
 
 
-outfile_ <- outfile[order(outfile$Fecha, outfile$Pluid, outfile$storeid, outfile$Hora), ]
+# ============ MEDICION DE ASERTIVIDAD ==========
+outfile_aux <- outfile_d[outfile_d$UnidadesVendidas.x != 0, ]
+#outfile_aux <- outfile_d
 
-aux[is.na(aux$UnidadesVendidas), 5] <- 0
+asertividad <- data.table(outfile_aux %>% group_by(SUBLINEA_ID, SUBLINEA_DESC, 
+                                                   CATEGORIA_ID, CATEGORIA_DESC, 
+                                                   SUBCATEGORIA_ID, SUBCATEGORIA_DESC,
+                                                   MARCA_ID, MARCA_DESC,
+                                                   Pluid, PLUCD, PLU_DESC,
+                                                   storeid, TIENDA_DC_DESC, CADENA_DESC, Fecha) %>% summarise(sum_mod1 = sum(UnidadesVendidas.x),
+                                                                                                       asert_mod1 = 1-100*sum(abs_proy)/sum(UnidadesVendidas.x),
+                                                                                                       sum_mod2 = sum(P1),
+                                                                                                       asert_mod2 = 100*(1-sum(abs_proy2)/sum(P1)),
+                                                                                                       sum_mod3 = sum(P2),
+                                                                                                       asert_mod3 = 100*(1-sum(abs_proy3)/sum(P2)),
+                                                                                                       sum_mod4 = sum(P3),
+                                                                                                       asert_mod4 = 100*(1-sum(abs_proy4)/sum(P3))))
+
+asertividad_subl <- data.table(outfile_aux %>% group_by(SUBLINEA_ID, SUBLINEA_DESC,storeid, TIENDA_DC_DESC, Fecha,  Hora) %>% summarise(sum_mod1 = sum(UnidadesVendidas.x),
+                                                                                                  asert_mod1 = 1-100*sum(abs_proy)/sum(UnidadesVendidas.x),
+                                                                                                  sum_mod2 = sum(P1),
+                                                                                                  asert_mod2 = 100*(1-sum(abs_proy2)/sum(P1)),
+                                                                                                  sum_mod3 = sum(P2),
+                                                                                                  asert_mod3 = 100*(1-sum(abs_proy3)/sum(P2)),
+                                                                                                  sum_mod4 = sum(P3),
+                                                                                                  asert_mod4 = 100*(1-sum(abs_proy4)/sum(P3))))
+
+asertividad_fecha <- data.table(outfile_aux %>% group_by(Fecha, Hora) %>% summarise(sum_mod1 = sum(UnidadesVendidas.x),
+                                                                                    asert_mod1 = 1-100*sum(abs_proy)/sum(UnidadesVendidas.x),
+                                                                                    sum_mod2 = sum(P1),
+                                                                                    asert_mod2 = 100*(1-sum(abs_proy2)/sum(P1)),
+                                                                                    sum_mod3 = sum(P2),
+                                                                                    asert_mod3 = 100*(1-sum(abs_proy3)/sum(P2)),
+                                                                                    sum_mod4 = sum(P3),
+                                                                                    asert_mod4 = 100*(1-sum(abs_proy4)/sum(P3))))
+
+
+asertividad_dep <- data.table(outfile_aux %>% group_by(storeid, TIENDA_DC_DESC) %>% summarise(sum_mod1 = sum(UnidadesVendidas.x),
+                                                                                              asert_mod1 = 1-100*sum(abs_proy)/sum(UnidadesVendidas.x),
+                                                                                              sum_mod2 = sum(P1),
+                                                                                              asert_mod2 = 100*(1-sum(abs_proy2)/sum(P1)),
+                                                                                              sum_mod3 = sum(P2),
+                                                                                              asert_mod3 = 100*(1-sum(abs_proy3)/sum(P2)),
+                                                                                              sum_mod4 = sum(P3),
+                                                                                              asert_mod4 = 100*(1-sum(abs_proy4)/sum(P3))))
+
+
+asertividad_hora <- data.table(outfile_aux %>% group_by(Hora) %>% summarise(sum_mod1 = sum(UnidadesVendidas.x),
+                                                                            asert_mod1 = 1-100*sum(abs_proy)/sum(UnidadesVendidas.x),
+                                                                            sum_mod2 = sum(P1),
+                                                                            asert_mod2 = 100*(1-sum(abs_proy2)/sum(P1)),
+                                                                            sum_mod3 = sum(P2),
+                                                                            asert_mod3 = 100*(1-sum(abs_proy3)/sum(P2)),
+                                                                            sum_mod4 = sum(P3),
+                                                                            asert_mod4 = 100*(1-sum(abs_proy4)/sum(P3))))
+
 # ================ ENVIAR E-MAIL DE FINALIZACIÓN DE PROCESO =============================
-
 
 send.mail(from = "jgomezz101@gmail.com",                                                     # Desde
           to = "jdgomezz@grupo-exito.com",                                                  # Para
